c488523be750a8ed1114c03ddfdced4e7f98563d
ToxicTerror21 <hussam.baradei@yahoo.com>
2022-07-11 23:40:48 -0400

    API Imports current weather desc as well as temp

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-
+const currentweather = document.getElementById("current");
+const currenttemp = document.getElementById("temp");
 start();
 function start(){
-fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=d107d94978e6221f163ee8546b7ae0a0')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=d107d94978e6221f163ee8546b7ae0a0', {mode: 'cors'})
   .then(function(response) {
-    // Successful response :)
-    alert("we did it");
+    return response.json();
   })
-  .catch(function(err) {
-    // Error :(
+  .then(function(data) {
+    currentweather.innerText = data.weather[0].description;
+    currenttemp.innerText = data.main.temp-273.15;
+    console.log(data.weather[0].description);
+    alert("we did it");
   });
 }
